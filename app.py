9ec8e9bb7863e68e5178503ec566d99ba42dbf64
Kavindu Dananjaya <42322531+Shazam007@users.noreply.github.com>
2021-08-31 19:55:12 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,85 @@
+from typing import Text
+import numpy as np
+from flask import Flask, request, jsonify, render_template
+import pickle
+import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.preprocessing import MultiLabelBinarizer
+
+# Create flask app
+flask_app = Flask(__name__)
+
+
+@flask_app.route("/rating", methods = ["GET","POST"])
+def rating():
+
+    #load the vectorizer
+    with open('vectorizer','rb') as f:
+        loaded_CV = pickle.load(f)
+
+    #load the trained models
+    with open('logReg_pickle','rb') as f:
+        lr = pickle.load(f)
+    with open('sgd_pickle','rb') as f:
+        sg = pickle.load(f)
+
+    recievedJsonFile = request.json
+
+    recievedText = recievedJsonFile["reviewText"]
+    inputText = [recievedText]
+
+    #inputText = []
+    
+    #fit input text to model
+    inputFeatures = loaded_CV.transform(inputText)
+
+    #choose the correct model here
+    predictions = lr.predict(inputFeatures)
+
+    #send results back
+    result = str(predictions[0])
+    
+    #return jsonify({"prediction":result})
+    return result
+
+@flask_app.route("/tags", methods = ["GET","POST"])
+def tags():
+    
+    #load the tfidf vectorizer
+    with open('TCvectorizer','rb') as f:
+        tfidf = pickle.load(f)
+
+    #load the classifier
+    with open('TC_pickle','rb') as f:
+        clf = pickle.load(f)
+
+    #load the binarizer
+    with open('multiLabelBiner','rb') as f:
+        multilabel = pickle.load(f)
+
+    #get the user input
+    recievedJsonFile = request.json
+
+    x = [recievedJsonFile["reviewText"]]
+    print(x)
+
+    #classification with input
+    #x = [ 'good for your children and crowded and fun']
+
+    vectors = tfidf.transform(x)
+    clf.predict(vectors)
+
+    tagsSet = multilabel.inverse_transform(clf.predict(vectors))
+    print(tagsSet[0])
+    
+
+    #return tagsSet
+    return jsonify(tagsSet[0])
+
+    #respective tags :  'calm', 'child', 'clean', 'crowded', 'family', 'food', 'fun'
+
+
+         
+
+if __name__ == "__main__":
+    flask_app.run(debug=True)
